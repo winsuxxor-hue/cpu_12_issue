d3c11a82c104a340d3c423e47c5c877fcaa34095
winsuxxor-hue <winsuxxor@gmail.com>
2026-09-08 12:14:10 +0300

    Refactor nested loops in task wb for clarity

--- WB.v
+++ WB.v
@@ -1,10 +1,18 @@
 task wb;
   integer c1;
   integer c2;
-  for(c1=0;c1<12;c1++)
-    for(c2=0;c2<12;c2=c2+1) begin
+  for(c1=0;c1<12;c1++) for(c2=0;c2<12;c2++)
+    for(c3=0;c3<12;c3++)
+    for(c4=0;c4<12;c4=c4+1)
+    for(f=0;f<12;f=f+1)
+    begin
+      for(a=0;a<3;a++)
       if (eng_free[f][c1][c2][a] && eng_ra[fu]
           [c3][c4][0]==treg && en)
         eng_free[f][c1][c2][a]<=0;
+      a=4;
+      if (eng_free[f][c1][c2][a] && eng_ra[fu]
+           [c3][c4][0]==treg2 && en2)
+         eng_free[f][c1][c2][a]<=0
     end
 endtask
